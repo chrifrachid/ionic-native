132601019392b4c8d862688753a8c084660560f0
Chrif Rachid <mr.bouderka.rachid@gmail.com>
2018-06-05 07:11:50 +0000

    Update index.ts

--- src/@ionic-native/plugins/admob-pro/index.ts
+++ src/@ionic-native/plugins/admob-pro/index.ts
@@ -103,7 +103,7 @@ export interface AdExtras {
  * @description
  * Plugin for Google Ads, including AdMob / DFP (DoubleClick for publisher) and mediations to other Ad networks.
  *
- * IMPORTANT NOTICE: this plugin takes a percentage out of your earnings if you profit more than $1,000. Read more about this on the plugin's repo. For a completely free alternative, see [AdMobPro Free](../admob-free).
+ * IMPORTANT NOTICE: this plugin takes a percentage out of your earnings if you profit more than $1,000. Read more about this on the plugin's repo. For a completely free alternative, see [AdMob Free](../admob-free).
  * @usage
  * ```typescript
  * import { AdMobPro } from '@ionic-native/admob-pro';
